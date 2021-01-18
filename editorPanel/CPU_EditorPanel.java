d74209ed7ff15deea2c36efd5d1a74e6d9b49d10
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-01-18 05:04:47 +0100

    CPU_Dialog

--- editorPanel/CPU_EditorPanel.java
+++ editorPanel/CPU_EditorPanel.java
@@ -25,13 +25,16 @@ public class CPU_EditorPanel extends JPanel implements ActionListener {
 
 	private static CPU_EditorPanel theController;
 	Vector<JButton> panelButtons;
+	Vector<JButton> panelButtons2;
 
 	//EditorMenu m;
 	Dialog d;
 
+	final String[] imageList = { "CPU_icona", "RAM_icona", "editor", "system", "queue", "setting"};
+	final String[] imageList2 = { "acc", "alu", "counter", "decoder", "exit", "instruction", "MUX"};
 	public CPU_EditorPanel() throws IOException {
 
-		final String[] imageList = { "CPU_icona", "RAM_icona", "editor", "system", "queue", "setting"};
+
 /*
 		m = new EditorMenu();
 		this.add(m.menu);
@@ -72,16 +75,27 @@ public class CPU_EditorPanel extends JPanel implements ActionListener {
 		Frame f = new Frame();
 		d = new Dialog(f, "CPU settings", true);
 		d.setLayout(new GridLayout(3,3));
-		JButton b = new JButton("OK");
-		b.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				f.setVisible(false);
+		d.setBackground(Color.lightGray);
+		panelButtons2 = new Vector<JButton>();
+
+		Icon icon;
+		JButton button8;
+		for (String image : imageList2) {
+			icon = new ImageIcon("../Sim_cpu/src/pippin/z__images3/" + image + ".png"); // ../Pippin_V003/src/pippin/z__images2/ir.png
+			button8 = new JButton(icon);
+			//button8.setBounds(0, 10, 200, 10);
+			button8.setBackground(Color.lightGray);
+
+			if(image == "exit") {
+				button8.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						f.setVisible(false);
+					}
+				});
 			}
-		});
-		d.add(b);
-		for(int i = 0; i<8; ++i) {
-			b = new JButton("-");
-			d.add(b);
+
+			d.add(button8);
+			panelButtons2.add(button8);
 		}
 		d.setBounds(370, 38, 360, 335);
 		d.setVisible(true);
