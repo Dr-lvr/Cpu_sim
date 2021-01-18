92d8143ba5645fb49ae363753e9b9c96316d104a
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-01-18 22:01:00 +0100

    Welcome panel

--- src/pippin/Main.java
+++ src/pippin/Main.java
@@ -5,8 +5,9 @@ import java.awt.*;
 		import java.awt.event.ActionListener;
 		import java.awt.event.MouseAdapter;
 		import java.awt.event.MouseEvent;
+import java.io.IOException;
 
-		import javax.swing.*;
+import javax.swing.*;
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////// BOOTSTRAP /////////////////////////////////////////////////////////////////////
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,55 +17,11 @@ import java.awt.*;
 
 		public class Main {
 			public static void main(String[] args) {
-
-
-					JFrame frame = new JFrame("CPU_Sim_Menu");
-					JPanel panel = new JPanel();
-					panel.setLayout(new CardLayout(350,300));
-					//panel.setBackground(Color.lightGray);
-					//JLabel label = new JLabel();
-					//label.setText("CPU_sim.V003");
-					//label.setBounds(0, 0, 100, 100);
-					Icon icon = new ImageIcon("../Sim_cpu/src/pippin/z__images2/mainMenuIcons/CPU_icona.png"); // ../Pippin_V003/src/pippin/pippin.z__images2/ir.png
-					JButton button = new JButton(icon);
-					button.setBackground(Color.orange);
-
-					button.addActionListener ( new ActionListener()
-					{
-						public void actionPerformed( ActionEvent e )
-						{
-							frame.setVisible(false);
-							try {
-								New_Sim newSim = new New_Sim();
-								newSim.setSize((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 500,
-										(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-								newSim.setTitle("New_Tuned_Simulator");
-								newSim.setVisible(true);
-							} catch (Exception c) {
-								c.printStackTrace();
-								System.exit(0);
-							}
-						}
-					});
-					button.addMouseListener(new MouseAdapter() {
-						@Override
-						public void mouseEntered(MouseEvent me) {
-							button.setBackground(Color.green);
-						}
-
-						@Override
-						public void mouseExited(MouseEvent me) {
-							button.setBackground(Color.orange);
-						}
-					});
-					panel.add(button);
-					frame.add(panel);
-					//frame.add(label);
-					frame.setBounds(0, 0, (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 500,
-							(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-					//frame.setLocationRelativeTo(null);
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setVisible(true);
+				try {
+					WelcomePanel pippin = new WelcomePanel();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ src/pippin/WelcomePanel.java
@@ -0,0 +1,72 @@
+    package pippin;
+
+    import javax.swing.*;
+    import java.awt.*;
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
+    import java.awt.event.MouseAdapter;
+    import java.awt.event.MouseEvent;
+    import java.io.IOException;
+
+    public class WelcomePanel extends JFrame {
+
+        public WelcomePanel() throws IOException {
+
+            super();
+            JPanel panel = new JPanel();
+            panel.setLayout(new CardLayout(350,300));
+            Icon icon = new ImageIcon("../Sim_cpu/src/pippin/z__images2/mainMenuIcons/CPU_icona.png"); // ../Pippin_V003/src/pippin/pippin.z__images2/ir.png
+            JButton button = new JButton(icon);
+            button.setBackground(Color.orange);
+
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setBackground(new Color(102, 102, 102));
+            setMinimumSize(new Dimension(0, 0));
+            setLayout(new FlowLayout());
+
+            JFrame frame = this;
+
+            button.addActionListener ( new ActionListener()
+            {
+                public void actionPerformed( ActionEvent e )
+                {
+                    frame.setVisible(false);
+                    try {
+                        WelcomePanel pippin = new WelcomePanel();
+                    } catch (IOException c) {
+                        c.printStackTrace();
+                    }
+                    try {
+
+                        New_Sim newSim = new New_Sim();
+                        newSim.setSize((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 500,
+                                (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+                        newSim.setTitle("New_Tuned_Simulator");
+                        newSim.setVisible(true);
+                    } catch (Exception c) {
+                        c.printStackTrace();
+                        System.exit(0);
+                    }
+                }
+            });
+            button.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent me) {
+                    button.setBackground(Color.green);
+                }
+
+                @Override
+                public void mouseExited(MouseEvent me) {
+                    button.setBackground(Color.orange);
+                }
+            });
+            panel.add(button);
+            frame.add(panel);
+            //frame.add(label);
+            frame.setBounds(0, 0, (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 500,
+                    (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+            //frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        }
+    }
