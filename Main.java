6957a442beea5b77ca697532a7b0d6a537c03dbc
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-01-18 05:34:22 +0100

    Welcome

--- Main.java
+++ Main.java
@@ -20,7 +20,10 @@
 				JFrame frame = new JFrame("CPU_Sim_Menu");
 				JPanel panel = new JPanel();
 				panel.setLayout(new CardLayout(350,300));
-				panel.setBackground(Color.lightGray);
+				//panel.setBackground(Color.lightGray);
+				//JLabel label = new JLabel();
+				//label.setText("CPU_sim.V003");
+				//label.setBounds(0, 0, 100, 100);
 				Icon icon = new ImageIcon("../Sim_cpu/src/pippin/z__images2/CPU_icona.png"); // ../Pippin_V003/src/pippin/z__images2/ir.png
 				JButton button = new JButton(icon);
 				button.setBackground(Color.orange);
@@ -44,6 +47,7 @@
 				});
 				panel.add(button);
 				frame.add(panel);
+				//frame.add(label);
 				frame.setBounds(0, 0, (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 500,
 						(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight());
 				//frame.setLocationRelativeTo(null);
